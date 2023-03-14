b7e1b697b573b2d4a094b5a8781e5de470754c64
Yun Young Choi <87300199+olsi10@users.noreply.github.com>
2023-03-14 19:55:23 +0900

    노드 시작 전 js 초기

---
+++ node1/test.js
@@ -0,0 +1,27 @@
+// console.err(), console.warn();
+
+console.log("여")
+console.log("수지다");
+console.log("문자열이다 %s", '안녕');
+
+var person = {
+    name: '수지',
+    age : 20
+};
+
+console.log("객체다 %j", person);
+console.dir(person);
+
+console.log("파일 이름 %s", __filename)
+console.log("파일 이름 %s", __dirname)
+
+let greeting = "안녕";
+let greeting2 = "바이";
+let agg = 20;
+const statement = `${greeting} my name is ${namee}`;
+const statement1 = `${greeting2} my name is ${namee}`;
+
+let fruit = "포도";
+console.log(fruit);
+fruit = "감자";
+console.log(fruit);
