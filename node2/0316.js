14a3fdc3730a7bd12bd2a60d36aa47d54265eac8
Yun Young Choi <87300199+olsi10@users.noreply.github.com>
2023-03-16 19:22:51 +0900

    process, console 등

---
+++ node2/0316.js
@@ -0,0 +1,65 @@
+// const num = [];
+// num.push(1);
+// num.push(2);
+// num.push(3);
+
+
+// for(let i of num) {
+//     console.log(num[i - 1]);
+// }
+
+// for(let i = 0; i < num.length; i++) {
+//     console.log(num[i]);
+// }
+
+// 자바스크립트 오브젝트 노테이션
+// 제이슨은 키와 값, 쌍으로 표현
+
+// const user = {};
+// user.name = '엉덩이';
+// user.age = 30;
+
+// console.log(user);
+
+// const user2 = {
+//     name:'엉덩이',
+//     age:30
+// };
+
+
+// console.log(user2);
+// console.log(`나의 이름은 ${user2.name}\n나이는${user2.age}\n학교는${user2.school}`);
+
+// const userer = 'age';
+// console.log(user2[userer]);
+
+// process 현재 동작중인 프로세스 실행에 대한 정보를 다루는 객체
+// env 환경변수 정보
+// require() 모듈 로딩 #include, import
+// export 모듈을 다루는 객체
+
+// console의 주요 메소드
+// console.dir(object) 객체 속성 출력
+// console.time(id) 실행 시간 측정 시작
+// console.timeEnd(id) 실행 시간 측정 끝
+
+// // PROCESS
+
+// console.log(process.env); // 컴퓨터 환경 관련 정보를 가진 아키텍쳐
+// console.log('========================================');
+// console.log(process.arch); // 프로세서의 아키텍쳐
+// console.log('========================================');
+// console.log(process.uptime()); // 현재 프로그램이 실행된 시간
+// console.log('========================================');
+// // console.log(process.memoryUseage()); // 메모리 사용 정보를 가진 객체
+// console.log(process.version); // Node.js 버전
+// console.log('========================================');
+// console.log(process.versions); // Node.js 버전
+// console.log('========================================');
+
+// // CONSOLE
+
+// console.log("Number : %d + %d = %d", 1, 2, 1 + 2);
+// console.log("String %s", 'Hello, world');
+// console.log("JSON %j", {name:'node.js'});
+// console.log("String 1", "String 2");
